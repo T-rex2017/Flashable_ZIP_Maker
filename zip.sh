0330875fc0d5f3c8d1f3afac3cbba8e55a717815
trex2017 <Tonykchristopher@gmail.com>
2018-07-21 16:52:19 +0530

    Kick Signer
    removed zip signing

--- zip.sh
+++ zip.sh
@@ -22,26 +22,17 @@ echo "Making Zip"
 # Making zip 
 cp -r ../tools/META-INF META-INF
 cp -r ../tools/system system
-cp ../tools/signapk.jar signapk.jar 
-cp ../tools/testkey.x509.pem testkey.x509.pem
-cp ../tools/testkey.pk8 testkey.pk8
 # zipping
 zip -r Flashable-ZIP-Maker.zip META-INF system  
 echo "ZIP is Ready"
-echo "Signing ZIP"
-java -jar signapk.jar testkey.x509.pem testkey.pk8 Flashable-ZIP-Maker.zip Flashable-ZIP-Maker-SIGNED.zip
 # Cleanup
 echo "Cleaning up"
-rm Flashable-ZIP-Maker.zip
-rm *.jar
-rm *.pk8
-rm *.pem
 rm -r META-INF 
 rm -r system
 cd ..
 #Zip Size
 cd output
-s1=`ls -lh Flashable-ZIP-Maker-SIGNED.zip | sed -e 's/.* [ ]*\([0-9]*\.[0-9]*[MK]\) .*/\1/g'`
+s1=`ls -lh Flashable-ZIP-Maker.zip | sed -e 's/.* [ ]*\([0-9]*\.[0-9]*[MK]\) .*/\1/g'`
 echo "Size of ZIP is = $s1"
 echo "Flashable zip is in output/Flashable-ZIP-Maker-SIGNED.zip"
 tput sgr0
@@ -50,6 +41,3 @@ echo "Flashable ZIP Maker by russelldias"
 sleep .1
 echo "Thanks for using Flashable ZIP Maker"
 sleep .1
-echo "Please press [Enter] to exit."
-tput sgr0
-read ANS
